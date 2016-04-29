720458a1d7cb3cb7ebd41477fef01b18213b10f0
wanghuacong <admin@goodbuild.cn>
2016-04-29 18:40:56 +0800

    add dialog file

---
+++ component/dialog.js
@@ -0,0 +1,14 @@
+'use strict';
+/**
+ * Created by whc on 16/4/29.
+ * dialog 组件
+ */
+var React = require('react-native');
+
+var Dialog = React.createClass({
+    render(){
+        return(<div></div>);
+    }
+});
+
+module.exprots = Dialog;
